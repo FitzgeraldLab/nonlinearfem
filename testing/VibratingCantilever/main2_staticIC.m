8725852af7933ecf2d6aadffbd2d48f940e95794
tim <fitzgeraldt@gonzaga.edu>
2021-06-11 15:20:06 -0700

    nearly have IC

--- testing/VibratingCantilever/main2_staticIC.m
+++ testing/VibratingCantilever/main2_staticIC.m
@@ -13,7 +13,6 @@ ROOTDIR = fullfile('../..');
 addpath(fullfile(ROOTDIR,'preprocmesh'));
 addpath(fullfile(ROOTDIR,'assemble'));
 addpath(fullfile(ROOTDIR,'postproc'));
-addpath(fullfile(ROOTDIR,'quadrature'));
 addpath(fullfile(ROOTDIR,'shapefunctions'));
 addpath(fullfile(ROOTDIR,'loadstepping'));
 
@@ -27,16 +26,17 @@ std_element_defs;
 max_Z = 0.1;
 
 % loading increment (negative or positive to pick direction)
-DP   = 2e-5;
+DP   = 2e-4;
 
 
 %% flags:
 flags.verbose   = true;
-flags.plot_steps_nskip = 4;
-field_range = nan;
+flags.plot_steps_nskip = 10;
+field_range = [0,max_Z*1.1];
 flags.plot_steps = 1;
 flags.plot_fancy = 1;
 flags.plot_colormap = 'default';
+flags.plot_trackPt = 1;
 
 %% Locate the nodal info to track
 % This is the displacement criteria to end the loop when max_Z is met:
@@ -47,7 +47,7 @@ qn = zeros(ndofs,1);
 if flags.plot_steps > 0
     [fig, ax, hlist] = plot_static_steps_init(qn, x, y, z, ID, msh, flags, ...
         max_Z, track_A);
-    
+    pause(0.01);
 end
 
 
@@ -61,7 +61,7 @@ Fext_load = 0;
 
 stepping.complete = false;
 stepping.iter = 0;
-stepping.iterMax = 26;
+stepping.iterMax = 5000;
 
 while stepping.complete == false
     
@@ -83,11 +83,13 @@ while stepping.complete == false
     if flags.plot_steps == 1 && mod(stepping.iter,flags.plot_steps_nskip) == 0
         hlist = plot_static_steps(ax, hlist, qn, x, y, z, ID, msh, ...
             flags, stepping.iter, ...
-            Fext_load, field_range);
+            Fext_load, field_range, track_A, track_idx);
     end
     
     %% check for end of loop:
-    if false
+    if qn(track_idx) >= max_Z
+        
+        stepping.complete = 1;
         
         
     elseif stepping.iter >= stepping.iterMax
@@ -96,3 +98,8 @@ while stepping.complete == false
     end
     
 end
+
+%% Save the output:
+outfile_datestr = datestr(now,'yyyy-mm-dd.HH-MM-SS');
+outfilename     = sprintf('data.%s.mat', outfile_datestr );
+save(outfilename, 'qn', 'Fext_load', 'stepping', 'info')
--- testing/VibratingCantilever/plot_static_steps.m
+++ testing/VibratingCantilever/plot_static_steps.m
@@ -1,11 +1,15 @@
 function hlist = plot_static_steps(ax, hlist, qn, x, y, z, ID, msh, flags, ...
-    n, load, field_range)
+    n, load, field_range, track_A, track_idx)
 
 ps_alpha = 0.5;
 
+%% clear the sheet:
 delete(hlist);
+
+%%
 [ps_nel, ps_eltype, ps_IEN] = parse_msh( msh, 'plot_surface');
 
+%%
 if flags.plot_fancy == 0
     hlist = plot_element_solution_hdsurf(ax, 1:ps_nel, 1, ...
         [0, 0, 1], ps_IEN, ID, ps_eltype, x, y, z, qn, ...
@@ -21,6 +25,19 @@ elseif flags.plot_fancy == 1
         'field_range', field_range);
 end
 
-title(sprintf('n=%5d, load=%-8.2e', n, load) );
+%% add tracking point
+if flags.plot_trackPt == 1
+    
+    temp = plot_node_solution(ax, ID, x, y, z, qn, 'A_in', track_A, ...
+        'markercolor', [0.8500, 0.3250, 0.0980], 'marker', 'o');
+    
+    hlist = [hlist, temp];
+    
+end
 
+
+%% Update the title
+title(sprintf('n=%5d, load=%-8.2e, dof=%3.1e', n, load, qn(track_idx)) );
+
+%% flush the buffer
 pause(0.01);
